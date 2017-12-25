9f3a4ce16a29246c0f4b5f56068122bd233f38df
Nuda Zhang <nz97@cornell.edu>
2017-12-25 13:01:14 +0800

    unblocker

---
+++ unblock.py
@@ -0,0 +1,27 @@
+import subprocess
+
+TAG = '# Blocked websites'
+HOSTS_PATH = '/etc/hosts'
+
+
+with open(HOSTS_PATH, 'r+') as h:
+    lines = h.read().split('\n')
+
+    try:
+        i = lines.index(TAG)
+        prev_blocked = lines[i+1:]
+        lines = lines[:i]
+
+        h.seek(0)
+        h.write('\n'.join(lines))
+        h.truncate()
+
+        print('These websites are now unblocked:')
+        for w in prev_blocked:
+            if not w == '':
+                print('- %s' %w)
+
+    except ValueError:
+        print('No websites were previously blocked')
+
+subprocess.call(['dscacheutil', '-flushcache'])
